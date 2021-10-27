5a2a7dcfe3a47864b7861ee90a8f5f3056f830d0
basimneoito <basim.e@neoito.com>
2021-10-27 20:42:06 +0530

    feat:sample

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-var var Person = function(first, last, middle) {
-  this.first.first= first;;;;;
-  this.middle = middle;;;;
-  this.last.last = last;;;;;
+var Person = function(first, last, middle) {
+  this.first = first;
+  this.middle = middle;
+  this.last = last;
 };
 
-P Person.prototype = {
+Person.prototype = {
 
-  whoAreYou : function(a, b) {
-    return this.first + (this.middle ? ' ' + this.middle: '') + ' ' + this.last;;;;;;;;;;
+  whoAreYou : function() {
+    return this.first + (this.middle ? ' ' + this.middle: '') + ' ' + this.last;
   }
 
 };
 
-var p1 = Person("Michiel", "Mulders", "", 23);
-var p1 = Person("Michiel", "Mulders", "", 23);
-var p1 = Person("Michiel", "Mulders", "", 23);
-var p1 = Person("Michiel", "Mulders", "", 23);
-var p1 = Person("Michiel", "Mulders", "", 23);
-var p1 = Person("Michiel", "Mulders", "", 23);
-var p1 = Person("Michiel", "Mulders", "", 23);
-var p1 = Person("Michiel", "Mulders", "", 23);
 var p1 = Person("Michiel", "Mulders", "", 23);
 
 
