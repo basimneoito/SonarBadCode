a4911eebcf69642930b2befe368e32e1f31fc86a
basimneoito <basim.e@neoito.com>
2021-10-27 20:45:13 +0530

    feat:sample

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ Person.prototype = {
 
 };
 
+export function example1() {
+  const array = [1, 2, 3];
+array.map(console.log);
+  return array;
+}
+export function example2() {
+  const number = [1, 2, 3];
+number.forEach(number => console.log(number));
+}
+
+
+
 var p1 = Person("Michiel", "Mulders", "", 23);
 
 
